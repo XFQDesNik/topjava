9d0be2d263f7bdd56986a254af09c6adb2355162
Artem Afonin <xfqdesnik@mail.ru>
2019-10-25 09:43:11 +0300

    Add CRUD and Layers

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -1,41 +1,48 @@
 package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
-public class MealTo {
-    private final LocalDateTime dateTime;
-    private final String description;
 
+public class MealTo {
+    private final Integer id;
+
+    private final LocalDateTime dateTime;
+
+    private final String description;
     private final int calories;
+
     private final boolean excess;
 
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
-        this.dateTime = dateTime;
-        this.description = description;
-        this.calories = calories;
-        this.excess = excess;
-    }
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
+    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+            this.id = id;
+            this.dateTime = dateTime;
+            this.description = description;
+            this.calories = calories;
+            this.excess = excess;
+        }
 
-    public String getDescription() {
-        return description;
-    }
+        public Integer getId() {
+            return id;
+        }
 
-    public int getCalories() {
-        return calories;
+        public LocalDateTime getDateTime() {
+            return dateTime;
+        }
+        public String getDescription() {
+            return description;
+        }
+        public int getCalories() {
+            return calories;
+        }
+        public boolean isExcess() {
+            return excess;
+        }
+        @Override
+        public String toString() {
+            return "MealTo{" +
+                    "id=" + id +
+                    ", dateTime=" + dateTime +
+                    ", description='" + description + '\'' +
+                    ", calories=" + calories +
+                    ", excess=" + excess +
+                    '}';
+        }
     }
-
-    public boolean isExcess() {
-        return excess;
-    }
-
-    @Override
-    public String toString() {
-        return "MealTo{" +
-                "dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                ", calories=" + calories +
-                ", excess=" + excess +
-                '}';
-    }
-}
