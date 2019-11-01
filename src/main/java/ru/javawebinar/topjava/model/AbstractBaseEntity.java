09d794b879eb6052e6709b03a84e329717b70600
Artem Afonin <xfqdesnik@mail.ru>
2019-11-01 11:25:45 +0300

    Add Spring Data JPA

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,44 +1,58 @@
 package ru.javawebinar.topjava.model;
 
-public abstract class AbstractBaseEntity {
-    public static final int START_SEQ = 100000;
+import org.hibernate.Hibernate;
+import org.springframework.data.domain.Persistable;
 
-    protected Integer id;
+import javax.persistence.*;
 
-    public AbstractBaseEntity() {
-    }
+@MappedSuperclass
+// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
+@Access(AccessType.FIELD)
 
-    protected AbstractBaseEntity(Integer id) {
-        this.id = id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public Integer getId() {
-        return id;
-    }
-    public boolean isNew() {
-        return this.id == null;
-    }
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + ":" + id;
-    }
+    public abstract class AbstractBaseEntity implements Persistable<Integer> {
+        public static final int START_SEQ = 100000;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
+        @Id
+        @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+//  See https://hibernate.atlassian.net/browse/HHH-3718 and https://hibernate.atlassian.net/browse/HHH-12034
+//  Proxy initialization when accessing its identifier managed now by JPA_PROXY_COMPLIANCE setting
+        protected Integer id;
+        protected AbstractBaseEntity() {
         }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
+        protected AbstractBaseEntity(Integer id) {
+            this.id = id;
         }
-        AbstractBaseEntity that = (AbstractBaseEntity) o;
-        return id != null && id.equals(that.id);
-    }
+        public void setId(Integer id) {
+            this.id = id;
+        }
 
-    @Override
-    public int hashCode() {
-        return id == null ? 0 : id;
+        @Override
+        public Integer getId() {
+            return id;
+        }
+
+        @Override
+        public boolean isNew() {
+            return this.id == null;
+        }
+        @Override
+        public String toString() {
+            return getClass().getSimpleName() + ":" + id;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+                return false;
+            }
+            AbstractBaseEntity that = (AbstractBaseEntity) o;
+            return id != null && id.equals(that.id);
+        }
+        @Override
+        public int hashCode() {
+            return id == null ? 0 : id;
+        }
     }
-} 
